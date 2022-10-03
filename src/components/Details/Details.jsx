4f8dcd957bb2c59e8baa2c7c7632be50f412d6c5
Brianna Oliveira <mystikgab@gmail.com>
2022-10-03 16:42:06 -0500

    Imported useHistory and added Details component to App.jsx

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,6 +2,10 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 function Details() {
+    const dispatch = useDispatch();
+    const movies = useSelector(store => store.movies);
+    const genres = useSelector(store => store.genres);
+    
     return
 }
 
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
+import { useHistory } from 'react-router-dom';
 
 function MovieList() {
 
